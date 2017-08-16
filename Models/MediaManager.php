2b15de1d7ff5a8f07e9c6e5c8b0a179217a8c75f
Venelin Iliev <venelin@provision.bg>
2017-08-16 15:35:56 +0300

    media manager download file when file is URL

--- Models/MediaManager.php
+++ Models/MediaManager.php
@@ -9,10 +9,10 @@ namespace ProVision\MediaManager\Models;
 
 use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 use ProVision\MediaManager\Traits\MediaManagerTrait;
 
-class MediaManager extends Model
-{
+class MediaManager extends Model {
     use \Rutorika\Sortable\SortableTrait, Translatable, MediaManagerTrait;
 
     protected static $sortableGroupField = [
@@ -38,23 +38,44 @@ class MediaManager extends Model
         'is_image' => 'boolean'
     ];
 
-    public static function boot()
-    {
+    public static function boot() {
         static::deleting(function ($model) {
             /*
              * automatic remove files
              */
-            if (\File::exists(public_path($model->path))) {
-                \File::deleteDirectory(public_path($model->path));
+            if (File::exists(public_path($model->path))) {
+                File::deleteDirectory(public_path($model->path));
             }
         });
 
         static::saving(function ($model) {
             if ($model->id) {
                 //automatic create directories structure
-                if (!\File::exists(public_path($model->path))) {
-                    \File::makeDirectory(public_path($model->path, 0775, true));
+                if (!File::exists(public_path($model->path))) {
+                    File::makeDirectory(public_path($model->path), 0775, true, true);
                 }
+
+
+            }
+        });
+
+        static::saved(function ($model) {
+
+            //automatic create directories structure
+            if (!File::exists(public_path($model->path))) {
+                File::makeDirectory(public_path($model->path), 0775, true, true);
+            }
+
+            /**
+             * Сваляне на файл - ако бъде подаден като ->file = url
+             */
+            if (filter_var($model->file, FILTER_VALIDATE_URL)) {
+                $contents = file_get_contents($model->file);
+                $fileSavePath = public_path($model->path . basename($model->file));
+                File::put($fileSavePath, $contents);
+                $model->file = basename($fileSavePath);
+                $model->save();
+                $model->quickResize();
             }
         });
 
@@ -64,8 +85,7 @@ class MediaManager extends Model
     /**
      * @return string
      */
-    public function getPathAttribute()
-    {
+    public function getPathAttribute() {
         $path = 'uploads' . DIRECTORY_SEPARATOR . 'media-manager' . DIRECTORY_SEPARATOR . '' . strtolower(str_ireplace('\\', '-', $this->attributes['mediaable_type']));
 
         //mediaable_sub_type
@@ -86,10 +106,10 @@ class MediaManager extends Model
      * Връща пълният път до файла - ако е картинка може да се подаде и размер
      *
      * @param string $size
+     *
      * @return string
      */
-    public function getPublicPath($size = false)
-    {
+    public function getPublicPath($size = false) {
         $path = str_ireplace('\\', '/', $this->path);
         if ($size && $this->is_image) {
             if ($size == '_') {
@@ -105,8 +125,7 @@ class MediaManager extends Model
     /**
      * quick resize media item
      */
-    public function quickResize()
-    {
+    public function quickResize() {
         return $this->resize($this);
     }
 }
